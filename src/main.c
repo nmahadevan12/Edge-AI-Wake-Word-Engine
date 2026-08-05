91b0b3d212b10294fa9a404507f7abccf712df6e
nmahadevan12 <nikhilmahadevan61@gmail.com>
2026-08-04 20:55:51 -0400

    custom stdint header file

--- src/main.c
+++ src/main.c
@@ -1,4 +1,14 @@
-#include <stdint.h>
+/* Unsigned Integer Types */
+typedef unsigned    char        uint8_t
+typedef unsigned    short       uint16_t
+typedef unsigned    long        uint32_t
+typedef unsigned    long long   uint64_t
+
+/* Signed Integer Types */
+typedef signed      char        int8_t
+typedef signed      short       int16_t
+typedef signed      long        int32_t
+typedef signed      long long   int64_t
 
 /* Base Addresses */
 #define PERIPH_BASE     (0x40000000U)
